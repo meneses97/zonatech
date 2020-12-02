b4e8dd5360f665eb2bd30b8374b201c0ee5f0d8e
meneses97 <adelitoramosmeneses@gmail.com>
2020-12-02 12:15:30 +0200

    android files

---
+++ app/src/main/assets/www/js/my-app.js
@@ -0,0 +1,74 @@
+// Initialize your app
+var myApp = new Framework7();
+
+// Export selectors engine
+var $$ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+    // Because we use fixed-through navbar we can enable dynamic navbar
+    dynamicNavbar: true
+});
+
+
+var mySwiper = myApp.swiper('.swiper-container', {
+  pagination: '.swiper-pagination',
+  paginationHide: false,
+  paginationClickable: true,
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+});
+
+$$(document).on('page:init', function (e) {
+  // Do something here when page loaded and initialized
+
+  myApp.alert('Each Pach You', "App Carga");
+})
+
+// In page events:
+$$(document).on('page:init', function (e) {
+  // Page Data contains all required information about loaded and initialized page
+  var page = e.detail;
+
+   if (page === 'services'){
+      myApp.alert("xxx", " "+e.detail);
+   }
+
+});
+
+// Callbacks to run specific code for specific pages, for example for About page:
+myApp.onPageInit('about', function (page) {
+    // run createContentPage func after link was clicked
+    $$('.create-page').on('click', function () {
+        createContentPage();
+    });
+});
+
+// Generate dynamic page
+var dynamicPageIndex = 0;
+function createContentPage() {
+	mainView.router.loadContent(
+        '<!-- Top Navbar-->' +
+        '<div class="navbar">' +
+        '  <div class="navbar-inner">' +
+        '    <div class="left"><a href="#" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>' +
+        '    <div class="center sliding">Dynamic Page ' + (++dynamicPageIndex) + '</div>' +
+        '  </div>' +
+        '</div>' +
+        '<div class="pages">' +
+        '  <!-- Page, data-page contains page name-->' +
+        '  <div data-page="dynamic-pages" class="page">' +
+        '    <!-- Scrollable page content-->' +
+        '    <div class="page-content">' +
+        '      <div class="content-block">' +
+        '        <div class="content-block-inner">' +
+        '          <p>Here is a dynamic page created on ' + new Date() + ' !</p>' +
+        '          <p>Go <a href="#" class="back">back</a> or go to <a href="services.html">Services</a>.</p>' +
+        '        </div>' +
+        '      </div>' +
+        '    </div>' +
+        '  </div>' +
+        '</div>'
+    );
+	return;
+}
